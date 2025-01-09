b1cc63d1efd3768261bb5c900d215c251889d42b
Luke Hopson <lphopson@icloud.com>
2025-01-09 01:23:40 -0500

    try select2

--- coin/src/main/resources/static/script.js
+++ coin/src/main/resources/static/script.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-$('select').selectpicker();
-})
+$(document).ready(function() {
+  $('#dropdown').select2({
+    dropdownAutoWidth: true,
+    width: '100%'
+  });
+});
 
 // https://www.youtube.com/watch?v=zUeG6VgdKSc
