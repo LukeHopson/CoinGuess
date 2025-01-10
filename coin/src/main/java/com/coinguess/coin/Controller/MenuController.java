47a2756309395a4d90fb0d2a2e9c10f9eeab6f3c
LukeHopson <125517615+LukeHopson@users.noreply.github.com>
2025-01-10 16:55:16 -0500

    Updated directory names

--- coin/src/main/java/com/coinguess/coin/Controller/MenuController.java
+++ coin/src/main/java/com/coinguess/coin/Controller/MenuController.java
@@ -1,4 +1,4 @@
-package com.coinguess.coin.zController;
+package com.coinguess.coin.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
